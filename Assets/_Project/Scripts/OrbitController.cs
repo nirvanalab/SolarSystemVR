403239333393f5c9e555dca20f53e1820af5330b
Vidhur Voora <lab.nirvana@gmail.com>
2018-03-24 22:52:17 -0700

    Initial commit

---
+++ Assets/_Project/Scripts/OrbitController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitController : MonoBehaviour {
+    public Transform OrbitPivot;
+    public float OrbitSpeed;
+    public float RotationSpeed;
+    public float DistanceFromStar;
+    public float PlanetRadius;
+
+	// Use this for initialization
+	void Start () {
+        Vector2 randomPosition = Random.insideUnitCircle;
+        transform.position = new Vector3(randomPosition.x, 0f, randomPosition.y) * DistanceFromStar;
+        transform.localScale = Vector3.one * PlanetRadius;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.RotateAround(OrbitPivot.position, Vector3.up, Time.deltaTime * OrbitSpeed);
+        this.transform.Rotate(Vector3.up, Time.deltaTime * RotationSpeed);
+	}
+}
